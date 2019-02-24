ae2d3423f9cf22ab89e841c26c5082281d47c5ea
Ezassefa <44813292+Ezassefa@users.noreply.github.com>
2019-02-23 21:12:40 -0500

    Add files via upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,37 @@
+var friends = [
+    {
+    "name":"Ahmed",
+    "photo":"https://m.media-amazon.com/images/M/MV5BMTI3NzEyNDExMF5BMl5BanBnXkFtZTcwNTY4MTIzMQ@@._V1_.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  
+}, {
+    "name": "Billy Bob",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMTcxOTUwMDI0OF5BMl5BanBnXkFtZTcwNjIyNzkzNw@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    "scores": [4,2,5,1,3,2,2,1,3,2]
+  }, {
+    "name": "Samuel L Jackson",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_.jpg",
+    "scores": [5,2,2,2,4,1,3,2,5,5]
+  }, {
+    "name": "Mario",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png",
+    "scores": [3,3,4,2,2,1,3,2,2,3]
+  }, {
+    "name": "Kim Kardashian",
+    "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kim-kardashian-west-celebrates-the-launch-of-kkw-beauty-on-news-photo-698979088-1549383888.jpg?crop=1.00xw:0.666xh;0,0.0955xh&resize=768:*",
+    "scores": [4,3,4,1,5,2,5,3,1,4]
+  }];
 
+
+  module.exports = friends;
